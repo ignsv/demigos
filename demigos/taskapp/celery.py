a096e1c9ebc1323887e94859cb091aca93aea7ca
Alex Ihnatenko <krylov1295@gmail.com>
2020-08-13 01:00:08 +0300

    Add celery beat functionality

--- demigos/taskapp/celery.py
+++ demigos/taskapp/celery.py
@@ -11,4 +11,14 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 app = Celery('demigos')
 app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS, force=True)
+app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    'recalculate-every-10-seconds': {
+        'task': 'demigos.crypto.tasks.recalculate_pair_volume',
+        'schedule': 10.0,
+    },
+}
+
+app.conf.broker_url = 'redis://redis:6379/1'
